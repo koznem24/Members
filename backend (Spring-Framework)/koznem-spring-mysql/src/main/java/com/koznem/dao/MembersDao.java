06a4e5e886869818f9d9ef8b258a64b9d20fccaa
Nemtullo <koznem24@gmail.com>
2021-02-27 19:19:22 +0100

    replacing back end

--- backend (Spring-Framework)/koznem-spring-mysql/src/main/java/com/koznem/dao/MembersDao.java
+++
@@ -1,9 +0,0 @@
-package com.koznem.dao;
-
-import com.koznem.model.*;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MembersDao extends CrudRepository<Members, Integer> {
-
-
-}
